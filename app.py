e0b644d9ddf81efe064392c19657fa697cfea811
Orakaro <nhatminh_179@hotmail.com>
2013-02-11 11:13:45 +0900

    contact

--- app.py
+++ app.py
@@ -5,7 +5,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Obama is the best!'
+    return """
+    contact me at vunhatminh@icloud.com 
+    """
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
